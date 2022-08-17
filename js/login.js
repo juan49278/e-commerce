0b088fb6095acd54db9886e1a39fe4fc5a3bce75
juan49278 <juan49278@gmail.com>
2022-08-17 02:58:18 -0300

    commit

--- js/login.js
+++ js/login.js
@@ -4,13 +4,16 @@ verifiedForm = addEventListener("submit",verified);
 function verified(e){
     let inputs = document.querySelectorAll("input");
     inputs.forEach(i => {
-        if(i.value == ""){
+        if(i.value.trim() == ""){
             i.classList.add("is-invalid")
         } else {
             i.classList.remove("is-invalid")
         }
     }); for(let i of inputs){
-        if(i.value == "")
-        return e.preventDefault()
+        if(i.value.trim() == "")
+        return e.preventDefault(), document.querySelector("label.pass-invalid").style.display="block", 
+        document.querySelector("label.pass").style.display="none",
+        document.querySelector("label.email-invalid").style.display="block",
+        document.querySelector("label.email").style.display="none"
     }
 }
