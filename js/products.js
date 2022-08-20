a7fe4087eb688fa00cce5ea65252be60b107ea2f
juan49278 <juan49278@gmail.com>
2022-08-20 20:55:04 -0300

    commit

--- js/products.js
+++ js/products.js
@@ -13,17 +13,18 @@ function showData(){
     for(let product of productsArray){
 prodContent.innerHTML += `
 <div onclick="setCatID(${product.id})" class="list-group-item list-group-item-action cursor-active">
-    <div class="row">
-        <div class="col-3">
+        <div class="row-3 d-flex">
+            <div class="col-3">
             <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
         </div>
         <div class="col">
             <div class="d-flex w-100 justify-content-between">
-                <h4 class="mb-1">${product.name}</h4>
+                <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost}</h4>
+                <div class="col">
+                </div>
                 <small class="text-muted">${product.soldCount} vendidos</small>
             </div>
             <p class="mb-1">${product.description}</p>
-        </div>
     </div>
 </div>`
 }
