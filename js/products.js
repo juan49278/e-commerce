c5b49abb8885826439a99a7904cb5406e950ecc2
juan49278 <juan49278@gmail.com>
2022-09-04 21:57:05 -0300

    entrega2

--- js/products.js
+++ js/products.js
@@ -1,34 +1,95 @@
-let jsons = localStorage.getItem('catID') 
-let autos = async () => {
-    let url = `https://japceibal.github.io/emercado-api/cats_products/${jsons}.json`;
+let jsons = localStorage.getItem('catID') ;
+let url = `https://japceibal.github.io/emercado-api/cats_products/${jsons}.json`;
+const ORDER_ASC_BY_PRECIO = "costUP"
+const ORDER_DESC_BY_PRECIO = "costDown"
+const ORDER_BY_RELEVANCY = "Price"
+let miniumPrice = undefined;
+let maxiumPrice = undefined;
+
+document.addEventListener('DOMContentLoaded',async () => {
     let promise = await fetch(url);
     let datos = await promise.json()
     productsList = datos.products
     let allCats = datos.catName
     showList(datos.products)
     document.getElementById('showCat').innerHTML = allCats
-}
-autos();
-    
-    
-    
+})
+
+document.getElementById("sortAsc").addEventListener("click", function(){
+    sortAndShowProducts("costUP", productsList);
+});
+
+document.getElementById("sortDesc").addEventListener("click", function(){
+    sortAndShowProducts("costDown", productsList);
+});
+
+document.getElementById("sortByCount").addEventListener("click", function(){
+    sortAndShowProducts("Price", productsList);
+});
+document.getElementById("clearFilter").addEventListener("click", function(){
+    document.getElementById("filterPriceMinium").value = "";
+    document.getElementById("filterPriceMaxium").value = "";
+
+    miniumPrice = undefined;
+    maxiumPrice = undefined;
+
+    showList();
+});
+
+document.getElementById("filterPrice").addEventListener("click", function(){
+    miniumPrice = document.getElementById("filterPriceMinium").value;
+    maxiumPrice = document.getElementById("filterPriceMaxium").value;
+
+    if ((miniumPrice != undefined) && (miniumPrice != "") && (parseInt(miniumPrice)) >= 0){
+        miniumPrice = parseInt(miniumPrice);
+    }
+    else{
+        miniumPrice = undefined;
+    }
+
+    if ((maxiumPrice != undefined) && (maxiumPrice != "") && (parseInt(maxiumPrice)) >= 0){
+        maxiumPrice = parseInt(maxiumPrice);
+    }
+    else{
+        maxiumPrice = undefined;
+    }
+
+    showList();
+});
     function showList(){
+        let toAppened = []
     for(let product of productsList){
-        document.getElementById('products-container').innerHTML += `
-<div class="list-group-item list-group-item-action cursor-active">
+        const productsContainer = document.getElementById('products-container')
+        if(((miniumPrice == undefined) || ((miniumPrice != undefined) && (product.cost >= miniumPrice))) &&
+        ((maxiumPrice == undefined) || ((maxiumPrice != undefined) && (product.cost <= maxiumPrice)))){
+        toAppened += `
+        <div class="products list-group-item list-group-item-action cursor-active">
         <div class="row-3 d-flex">
-            <div class="col-3 d-flex">
-            <img src="${product.image}"alt="${product.description}" class="img-thumbnail">
-        </div>
-        <div class="col">
-            <div class="d-flex w-100 justify-content-between">
-                <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost}</h4>
-                <div class="col">
-                </div>
-                <small class="text-muted">${product.soldCount} vendidos</small>
+            <div class="col-3">
+                <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
             </div>
-            <p class="mb-1">${product.description}</p>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost}</h4>
+                    <small class="text-muted">${product.soldCount} vendidos</small>
+                </div>
+                <p class="mb-1">${product.description}</p>
+            </div>
+        </div>
     </div>
-</div>`
+    `
+        }
+        productsContainer.innerHTML = toAppened
+    }
 }
+
+function sortAndShowProducts(criteria, elements){
+    
+    const order = {
+        "costUP": () => elements.sort(function(a, b) { return a.cost - b.cost }),
+        "costDown": () => elements.sort(function(a, b) { return b.cost - a.cost}),
+        "Price": () => elements.sort(function(a, b){ return b.soldCount - a.soldCount}),      
+    }
+    productsList = order[criteria]();
+    showList();
 }
