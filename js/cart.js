d184a1c30ad749ff512890e80248a7ff25684fc8
juan49278 <juan49278@gmail.com>
2022-11-10 00:22:35 -0300

    update-profile

--- js/cart.js
+++ js/cart.js
@@ -43,10 +43,10 @@ function showCart(list) {
         <td class="text-muted"><span class="money">${currency}</span> <span class="cost">${unitCost}</span></td>
         <td><input type="number" placeholder="1" name="unit" value="1" min="1" class="form-control count" oninput="calcular()"></td>
         <td class="currency"><strong>USD <span class="result">${unitCost}</strong></span></td>
-        <td><svg onclick="deleteItem(${id}),calcular()" data-bs-toggle="tooltip" data-bs-placement="top" title="Eliminar producto" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
+        <td><svg onclick="deleteItem(${id}),calcular()" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
   <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-</svg></td>
+  data-bs-toggle="tooltip" data-bs-placement="top" title="Eliminar producto" class="cursor-active"></svg></td>
         </tr>`
     }
 }
